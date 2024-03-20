d607b53af40078efe5df837e73bf4a71a782fc4b
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2024-03-20 14:08:00 +0000

    v2 start 9:07 3/20/24

---
+++ Current (Select Folder)/Scripts/ContentScript.js
@@ -0,0 +1,8 @@
+// Create a new HTML element.
+var div = document.createElement("div");
+
+// Set the HTML element's content.
+div.innerHTML = "Hello, world!";
+
+// Append the HTML element to the body of the page.
+document.body.appendChild(div);
